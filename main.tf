860745d259a12947dcf1646748cbdfb7e2a5e9a5
dimi4ik <dima@lejkin.de>
2024-07-26 19:10:29 +0000

    add ddc

--- main.tf
+++ main.tf
@@ -40,3 +40,14 @@ resource "citrix_stf_webreceiver_service" "example-stf-webreceiver-service" {
     html5_enabled           = "Off"
   }
 }
+
+
+
+resource "citrix_stf_store_farm" "example-stf-store-farm" {
+    store_virtual_path      = citrix_stf_store_service.example-stf-store-service.virtual_path
+    farm_name = "Controller1"
+    farm_type = "XenDesktop"
+    servers = ["cvad.storefront.com"] 
+    port = 88
+    zones = ["Primary","Secondary", "Thirds"]
+}
