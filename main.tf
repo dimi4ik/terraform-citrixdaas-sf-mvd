1d6f06e05ea371660ce2644a06622ec1e83ae3b6
Dmitrij Lejkin <dmitrij.lejkin@abraxas.ch>
2024-07-26 21:49:37 +0200

    Updated main.tf with plugin_assistant configuration and citrix_stf_store_farm resource

--- main.tf
+++ main.tf
@@ -17,11 +17,31 @@ resource "citrix_stf_store_service" "example-stf-store-service" {
   virtual_path                        = var.virtual_path
   friendly_name                       = "Store"
   authentication_service_virtual_path = citrix_stf_authentication_service.example-stf-authentication-service.virtual_path
-  #farm_config = {
-  #  farm_name = "Controller"
-  #  farm_type = "XenDesktop"
-  # servers   = ["cvad.storefront.com"]
-  #}
+
+  pna = {
+    enabled = true
+  }
+  enumeration_options = {
+    enhanced_enumeration            = false
+    maximum_concurrent_enumerations = 2
+    filter_by_keywords_include      = ["AppSet1", "AppSet2"]
+  }
+  launch_options = {
+    vda_logon_data_provider = "FASLogonDataProvider"
+  }
+  farm_settings = {
+    enable_file_type_association   = true
+    communication_timeout          = "0.0:0:0"
+    connection_timeout             = "0.0:0:0"
+    leasing_status_expiry_failed   = "0.0:0:0"
+    leasing_status_expiry_leasing  = "0.0:0:0"
+    leasing_status_expiry_pending  = "0.0:0:0"
+    pooled_sockets                 = false
+    server_communication_attempts  = 5
+    background_healthcheck_polling = "0.0:0:0"
+    advanced_healthcheck           = false
+    cert_revocation_policy         = "MustCheck"
+  }
 }
 
 
