0220d665d490f3c9e046d6a41ce7a4c5083b546f
dimi4ik <dima@lejkin.de>
2024-07-26 19:28:38 +0000

    add_sf_config

--- main.tf
+++ main.tf
@@ -17,12 +17,39 @@ resource "citrix_stf_store_service" "example-stf-store-service" {
   virtual_path                        = var.virtual_path
   friendly_name                       = "Store"
   authentication_service_virtual_path = citrix_stf_authentication_service.example-stf-authentication-service.virtual_path
-  #farm_config = {
-  #  farm_name = "Controller"
-  #  farm_type = "XenDesktop"
-  # servers   = ["cvad.storefront.com"]
+
+  #pna = {
+  #  enabled = true
   #}
+  enumeration_options = {
+    enhanced_enumeration            = false
+    maximum_concurrent_enumerations = 2
+    filter_by_keywords_include      = ["AppSet1", "AppSet2"]
+  }
+  launch_options = {
+    vda_logon_data_provider = "FASLogonDataProvider"
+  }
+  /*
+  farm_settings = {
+    enable_file_type_association   = true
+    communication_timeout          = "0.0:0:0"
+    connection_timeout             = "0.0:0:0"
+    leasing_status_expiry_failed   = "0.0:0:0"
+    leasing_status_expiry_leasing  = "0.0:0:0"
+    leasing_status_expiry_pending  = "0.0:0:0"
+    pooled_sockets                 = false
+    server_communication_attempts  = 5
+    background_healthcheck_polling = "0.0:0:0"
+    advanced_healthcheck           = false
+    cert_revocation_policy         = "MustCheck"
+  }
+  */
+  gateway_settings = {
+    enable      = true
+    gateway_url = "https://xsl-ddc.ctx-ad.local"
+  }
 }
+
 
 
 resource "citrix_stf_webreceiver_service" "example-stf-webreceiver-service" {
@@ -50,6 +77,6 @@ resource "citrix_stf_store_farm" "example-stf-store-farm" {
   farm_name          = "Controller1"
   farm_type          = "XenDesktop"
   servers            = var.servers_sta
-  port               = 88
+  port               = 443
   zones              = ["Primary", "Secondary", "Thirds"]
 }
